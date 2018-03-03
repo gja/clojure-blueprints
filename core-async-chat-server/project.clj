57878127bfa12ee61c4a0305701493ecb4426a62
Tejas Dinkar <tejas@gja.in>
2018-03-03 23:41:39 +0530

    Simple chat server

--- core-async-chat-server/project.clj
+++ core-async-chat-server/project.clj
@@ -4,8 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [ring "1.6.3"]
-                 [commons-codec "1.11"]]
+                 [org.clojure/core.async "0.4.474"]
+                 [cheshire "5.8.0"]
+                 [info.sunng/ring-jetty9-adapter "0.10.4"]]
   :plugins [[lein-ring "0.12.1"]]
   :ring {:handler core-async-chat-server.core/ring-handler
          :auto-reload? true
--- core-async-chat-server/src/core_async_chat_server/core.clj
+++ core-async-chat-server/src/core_async_chat_server/core.clj
@@ -1,27 +1,47 @@
 (ns core-async-chat-server.core
   (:import [org.apache.commons.codec.digest DigestUtils]
            [org.apache.commons.codec.binary Base64])
+  (:require [ring.adapter.jetty9 :refer [run-jetty]]
+            [ring.adapter.jetty9.websocket :as ws]
+            [cheshire.core :as json])
   (:gen-class))
 
-(set! *warn-on-reflection* true)
+(def ws-handler {})
 
-(defn- sha1 [^String str]
-  (-> str
-      DigestUtils/sha1
-      Base64/encodeBase64String))
+(defn app [request]
+  {:status 200
+   :body "pong"})
 
-(defn route-request [{:keys [body uri request-method headers] :as request}]
-  (case [request-method uri]
-    [:get "/websocket"]
-    {:status 101
-     :headers {"Upgrade" "websocket"
-               "Connection" "upgrade"
-               "Sec-WebSocket-Accept" (-> (headers "sec-websocket-key")
-                                          (str "258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
-                                          sha1)}}
+(def channel-registry
+  (atom {:channel-to-sockets {}
+         :socket-to-channels {}}))
 
-    {:status 404
-     :body (str request-method " " uri " not found")}))
+(defn- add-to-set [set item]
+  (conj (or set #{})
+        item))
 
-(def ring-handler
-  (-> route-request))
+(defn- process-message [ws {:keys [type channel] :as m}]
+  (case type
+    "join" (do (swap! channel-registry update-in [:channel-to-sockets channel] add-to-set ws)
+               (swap! channel-registry update-in [:socket-to-channels ws] add-to-set channel)
+               (prn (str "Joining Channel:" channel)))
+    "msg" (let [message (json/encode {:message (:message m)})]
+            (doseq [socket (get-in @channel-registry [:channel-to-sockets channel])]
+              (ws/send! socket message)))
+    "leave" (swap! channel-registry update-in [:socket-to-channels ws] disj channel)))
+
+(defn- remove-socket [ws status-code reason]
+  (prn "Closing Socket")
+  (let [channels (get-in @channel-registry [:socket-to-channels ws] #{})]
+    (swap! channel-registry update-in [:socket-to-channels] dissoc ws)
+    (doseq [channel channels]
+      (swap! channel-registry update-in [:channel-to-sockets channel] disj ws))))
+
+(defn -main [& args]
+  (run-jetty app {:port 3000
+                  :websockets {"/connect" {:on-connect (fn [ws]
+                                                         (prn "connected"))
+                                           :on-text (fn [ws text]
+                                                      (process-message ws (json/decode text keyword)))
+                                           :on-close remove-socket}}
+                  :allow-null-path-info true}))
